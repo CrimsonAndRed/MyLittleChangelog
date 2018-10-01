1c5971a4c241860dd0bdac7cafefa299e235d7ad
Danone <devochkanyashka@yandex.com>
2018-10-01 21:46:07 +0300

    Basic react frontend.

---
+++ src/web/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import _ from 'lodash'
+
+import './assets/css/default.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	sdsd
+      </div>
+    );
+  };
+
+  componentDidMount(){
+  	console.log(_);
+  }
+}
+
+export default App;
