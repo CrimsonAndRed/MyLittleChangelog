38898ecabb57b1a4e28f826afb59f749d75c1840
Danone <devochkanyashka@yandex.com>
2018-10-04 23:41:38 +0300

    React router playing.

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import _ from 'lodash';
+import MainRouter from './components/main/MainRouter';
 
 import './assets/css/main.min.css';
 
+// Route urls to MainRouter component, so they are wrapped with "pretty" menu header and sides align
+// Any route, than does not need menu and sides align, goes before <Route path='/'/>
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      	sdsd
-      </div>
+        <Switch>
+          <Route path='/' component={MainRouter}/>
+        </Switch>
     );
   };
 
---
+++ src/web/src/components/main/MenuHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MenuHeader extends Component {
+  render() {
+    return (
+        <div id="main-header">
+          I am menu
+        </div>
+    );
+  };
+}
+
+export default MenuHeader;
