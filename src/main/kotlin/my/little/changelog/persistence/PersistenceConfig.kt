505b6e55093263fcd46f74da6f4c0d0760467ad2
PStroganov <petr.stroganov@gmail.com>
2020-09-05 13:28:24 +0300

    Добавление конфигурации БД в application.conf

--- src/main/kotlin/my/little/changelog/persistence/PersistenceConfig.kt
+++ src/main/kotlin/my/little/changelog/persistence/PersistenceConfig.kt
@@ -3,14 +3,22 @@ package my.little.changelog.persistence
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
+import io.ktor.config.*
+import io.ktor.util.*
 import org.jetbrains.exposed.sql.Database
 
+@KtorExperimentalAPI
 fun Application.module() {
-    initDb()
+    initDb(environment.config.config("database"))
 }
 
-fun initDb() {
-    val hikariConfig = HikariConfig("/hikari.properties")
+@KtorExperimentalAPI
+fun initDb(conf: ApplicationConfig) {
+    val hikariConfig = HikariConfig().apply {
+        jdbcUrl = conf.property("url").getString()
+        username = conf.property("username").getString()
+        password = conf.property("password").getString()
+    }
     val hikariDataSource = HikariDataSource(hikariConfig)
     Database.connect(hikariDataSource)
 }
