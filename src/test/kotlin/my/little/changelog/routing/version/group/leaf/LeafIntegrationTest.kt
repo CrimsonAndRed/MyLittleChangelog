796b7151a32fd95c6427c3447a37b618ad7e92c0
PStroganov <petr.stroganov@gmail.com>
2020-09-13 18:24:41 +0300

    Добавление теста на валидацию версии при удалении лифа

--- src/test/kotlin/my/little/changelog/routing/version/group/leaf/LeafIntegrationTest.kt
+++ src/test/kotlin/my/little/changelog/routing/version/group/leaf/LeafIntegrationTest.kt
@@ -20,6 +20,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertNotNull
 
 @KtorExperimentalAPI
 internal class LeafIntegrationTest : AbstractIntegrationTest() {
@@ -182,6 +183,38 @@ internal class LeafIntegrationTest : AbstractIntegrationTest() {
                     assertEquals(HttpStatusCode.NoContent, response.status())
                 }
                 assertThrows<EntityNotFoundException> { Leaf[leaf.id] }
+            }
+        }
+    }
+
+    @Test
+    fun `Test Delete Leaf with Wrong Version`() {
+        testApplication {
+            transaction {
+                val version1 = Version.new {}
+                val version2 = Version.new {}
+                val group = Group.new {
+                    this.version = version1
+                    this.name = "Группа1"
+                    this.parentVid = null
+                }
+                commit()
+
+                val leaf = Leaf.new {
+                    this.valueType = 1
+                    this.value = "Значение1"
+                    this.version = version1
+                    this.name = "Лиф1"
+                    this.groupVid = group.vid
+                }
+                commit()
+
+                with(
+                    handleRequest(HttpMethod.Delete, "/version/${version2.id}/group/${group.id}/leaf/${leaf.id}")
+                ) {
+                    assertEquals(HttpStatusCode.InternalServerError, response.status())
+                }
+                assertNotNull(Leaf[leaf.id])
             }
         }
     }
