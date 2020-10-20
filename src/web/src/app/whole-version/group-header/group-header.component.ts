39057fe8a44eb1c5e952abedc9d41e56c7bd8c2a
PStroganov <petr.stroganov@gmail.com>
2020-10-20 22:06:09 +0300

    Работает даже штота

---
+++ src/web/src/app/whole-version/group-header/group-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { GroupContent, NewGroupWithId } from 'app/model/group-content';
+
+@Component({
+  selector: 'group-header',
+  templateUrl: './group-header.component.html',
+  styleUrls: ['./group-header.component.scss']
+})
+export class GroupHeader {
+
+  constructor() { };
+
+}
