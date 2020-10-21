36e38a855eb123b2d15a10c6d3a1e2faa3929d96
PStroganov <petr.stroganov@gmail.com>
2020-10-21 18:18:55 +0300

    Рабочее добавление лифов в новой верстке

--- src/web/src/app/whole-version/group-header/group-header.component.ts
+++ src/web/src/app/whole-version/group-header/group-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { GroupContent, NewGroupWithId } from 'app/model/group-content';
+import { LeafContent, NewLeafWithId, UpdatedLeaf } from 'app/model/leaf-content';
 import { GroupHeader } from 'app/groups-sec/groups-sec.model';
 
 @Component({
@@ -24,6 +25,19 @@ export class GroupHeaderComponent implements GroupHeader {
             leafContent: []
           };
     this.group.groupContent.push(newGroup);
+    this.onGroupChange.emit(this.group);
+  }
+
+  handleNewLeaf(newLeafWithId: NewLeafWithId) {
+    const newLeaf: LeafContent = {
+      id: newLeafWithId.id,
+      name: newLeafWithId.name,
+      vid: newLeafWithId.vid,
+      valueType: newLeafWithId.valueType,
+      value: newLeafWithId.value,
+      groupVid: newLeafWithId.groupVid
+    }
+    this.group.leafContent.push(newLeaf);
     this.onGroupChange.emit(this.group);
   }
 
