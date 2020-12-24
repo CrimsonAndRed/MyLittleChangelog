aaeb8b1a95baabab55554a6c4e5b5622d3992695
Danone <crimsonandred@mail.ru>
2020-12-24 13:21:41 +0300

    Добавлены тесты на мув группы

--- src/test/kotlin/my/little/changelog/routing/version/group/GroupIntegrationTest.kt
+++ src/test/kotlin/my/little/changelog/routing/version/group/GroupIntegrationTest.kt
@@ -2,16 +2,21 @@ package my.little.changelog.routing.version.group
 
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationEngine
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import my.little.changelog.configuration.Json
+import my.little.changelog.model.group.Group
+import my.little.changelog.model.group.dto.external.ChangeGroupPositionDto
 import my.little.changelog.model.group.dto.external.GroupCreationDto
 import my.little.changelog.model.group.dto.external.GroupUpdateDto
 import my.little.changelog.model.group.dto.external.ReturnedGroupDto
 import my.little.changelog.model.leaf.Leaf
+import my.little.changelog.model.version.Version
+import my.little.changelog.persistence.repo.GroupRepo
 import my.little.changelog.routing.AbstractIntegrationTest
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -311,6 +316,57 @@ internal class GroupIntegrationTest : AbstractIntegrationTest() {
                     assertEquals(HttpStatusCode.InternalServerError, response.status())
                 }
             }
+        }
+    }
+
+
+    @Test
+    fun `Test Move group`() {
+        testApplication {
+            transaction {
+                val version = createVersion()
+                val parentGroup = createGroup(version)
+                val group1 = createGroup(version, parentGroup.vid)
+                val group2 = createGroup(version, parentGroup.vid)
+                testMoveGroupPositive(version, group1, group2)
+            }
+            transaction {
+                val version = createVersion()
+                val parentGroup = createGroup(version)
+                val group1 = createGroup(version, parentGroup.vid)
+                val group2 = createGroup(version, parentGroup.vid)
+                testMoveGroupPositive(version, group2, group1)
+            }
+            transaction {
+                val version = createVersion()
+                val parentGroup1 = createGroup(version)
+                val parentGroup2 = createGroup(version)
+                val group1 = createGroup(version, parentGroup1.vid)
+                val group2 = createGroup(version, parentGroup2.vid)
+                testMoveGroupNegative(version, group1, group2)
+            }
+        }
+    }
+
+    private fun TestApplicationEngine.testMoveGroupPositive(version: Version, group1: Group, group2: Group) {
+        val dto = ChangeGroupPositionDto(group2.id.value)
+        handleRequest(HttpMethod.Patch, "/version/${version.id}/group/${group1.id}/position") {
+            addHeader("Content-Type", "application/json")
+            setBody(Json.encodeToString(dto))
+        }.apply {
+            assertEquals(HttpStatusCode.NoContent, response.status())
+            assertEquals(GroupRepo.findById(group1.id.value).order, group2.order)
+            assertEquals(GroupRepo.findById(group2.id.value).order, group1.order)
+        }
+    }
+
+    private fun TestApplicationEngine.testMoveGroupNegative(version: Version, group1: Group, group2: Group) {
+        val dto = ChangeGroupPositionDto(group2.id.value)
+        handleRequest(HttpMethod.Patch, "/version/${version.id}/group/${group1.id}/position") {
+            addHeader("Content-Type", "application/json")
+            setBody(Json.encodeToString(dto))
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, response.status())
         }
     }
 }
